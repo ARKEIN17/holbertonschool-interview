7a29e722ccf7717c87bb7050ecd3650508469d2f
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-12-11 10:09:04 -0500

    task 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-"""method that determines if all the boxes can be opened."""
+
+"""You have n number of locked boxes in front
+ of you. Each box is numbered sequentially
+ from 0 to n - 1 and each box may contain keys to the other boxes.
+"""
 
 
 def canUnlockAll(boxes):
-    unlocked = [0]
-    for index, box in enumerate(boxes):
-        if not box:
-            continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked and key != index:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+    """
+    function that checks if the value in the list exists.
+    """
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True

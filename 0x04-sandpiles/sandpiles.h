d42e0e8aa1c14c57cab526c04cdc8129ae711001
ARKEIN17 <ramirez1kevin98@gmail.com>
2023-01-15 20:19:28 -0500

    changes

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,13 +1,10 @@
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
+void grid_addition(int grid1[3][3], int grid2[3][3]);
+void grid_print(int grid[3][3]);
+int grid_is_ok(int grid[3][3]);
+void grid_change(int grid1[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
-void sum_grids(int grid1[3][3], int grid2[3][3]);
-void change_grids(int grid1[3][3]);
-int check_grids(int grid1[3][3]);
 
-#endif
+#endif /* SANDPILES_H */
